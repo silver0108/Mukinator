ce24ef9a3029a11d0c7e8751f14c1ea2b7e353df
Jia <jia5232@naver.com>
2022-06-21 14:16:35 +0900

    pagination

--- mukinator/community/urls.py
+++ mukinator/community/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:post_id>/', views.detail, name='detail'),
     path('comment/create/<int:post_id>/', views.comment_create, name='comment_create'),
+    path('post/create/', views.post_create, name='post_create'),
 ]
--- mukinator/community/views.py
+++ mukinator/community/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from django.http import HttpResponseNotAllowed
+from .forms import PostForm, CommentForm
+from django.core.paginator import Paginator
 
 
 def index(request):
+    page = request.GET.get('page', 1)
     post_list = Post.objects.order_by('-create_date')
-    context = {'post_list': post_list}
+    paginator = Paginator(post_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'post_list': page_obj}
     return render(request, 'community/post_list.html', context)
 
 
@@ -17,5 +23,31 @@ def detail(request, post_id):
 
 def comment_create(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    post.comment_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('community:detail', post_id=post_id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.create_date = timezone.now()
+            comment.post = post
+            comment.save()
+            return redirect('community:detail', post_id=post.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible')
+    context = {'post': post, 'form':form}
+    return render(request, 'community/post_detail.html', context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.create_date = timezone.now()
+            post.save()
+            return redirect('community:index')
+    else:
+        form = PostForm()
+    context = {'form': form}
+    return render(request, 'community/post_form.html', context)
+
+
