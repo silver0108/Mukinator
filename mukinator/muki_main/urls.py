feb186dd192d7d17f3259f26a3f8d78ad4c2c7f9
mumat0103 <wsx2138@naver.com>
2022-06-23 14:49:20 +0900

    sort_food 코드 간결화, request POST로 변경

--- mukinator/muki_main/urls.py
+++ mukinator/muki_main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('sort_food', views.sort_food, name="sort_food"),
     path('start', views.start, name="start"),
+    path('reset', views.reset, name="reset"),
+    path('sort_food', views.sort_food, name="sort_food"),
+    path('go_borad', views.go_board, name="go_board"),
 ]
