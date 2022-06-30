ee0d68fd39b05db3ade06a683eadc3d63b296a30
Jia <jia5232@naver.com>
2022-06-30 11:45:15 +0900

    이메일 오류 수정

--- mukinator/common/models.py
+++ mukinator/common/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     nickname = models.CharField(max_length=10)
     region = models.CharField(max_length=20)
+    email = models.EmailField(max_length=45, unique=True)
